e48e58247068a99726e1e01bc58d819947c1274c
AmeriCanAndroid <americanandroidrom@gmail.com>
2013-02-02 11:32:33 -0700

    lol wow.. good morning.

--- msm8660.mk
+++ msm8660.mk
@@ -39,7 +39,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio_policy.msm8660 \
-    audio_policy.conf \
     audio.primary.msm8660 \
     libaudioutils \
     audio.usb.default \
